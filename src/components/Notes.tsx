ace3136427308c589fd06b50b5e93bd8801cc074
iddqdkfa <aaronz@telus.net>
2022-06-23 19:29:25 -0700

    Added filtering to Your Notes page.  Should also be able to use it for the search page

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,6 +1,6 @@
 import { faFilePdf, faImage } from "@fortawesome/free-solid-svg-icons";
 import { useDispatch, useSelector } from "react-redux";
-import { courses } from "../constants/courses";
+import { courses, defaultOptions } from "../constants/courses";
 import Navbar from "./Navbar";
 import NoteGrid from "./NoteGrid";
 import { Note } from "./Noteteaser";
@@ -10,12 +10,8 @@ export default function Notes() {
   const dispatch = useDispatch()
   let noteArr  = useAppSelector(state => state.userNotes)
 
- const options = [
-  { label: 'CPSC 310', id: 1 },
-  { label: 'ECON 101', id: 2 },
-];
     return (<div style={{backgroundColor: "white"}}>
       <Navbar/>
-      <NoteGrid notes = {noteArr} options = {options}/>
+      <NoteGrid notes = {noteArr} options = {defaultOptions}/>
     </div>)
   }
