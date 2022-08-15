30532852e78c2ddf06be4fc4e2f2d44c0f672fd0
iddqdkfa <aaronz@telus.net>
2022-08-15 16:02:49 -0700

    Fix deleted firebase issue

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,9 +1,9 @@
 export default {
-  apiKey: 'AIzaSyD77bItNXnMrKHnWwnAkdj0FEFzql1cztU',
-  authDomain: 'cpsc455-notes-app.firebaseapp.com',
-  projectId: 'cpsc455-notes-app',
-  storageBucket: 'cpsc455-notes-app.appspot.com',
-  messagingSenderId: '292570927581',
-  appId: '1:292570927581:web:6856d9c57a0f08d10a6634',
-  measurementId: 'G-NFZNMB8938',
+  apiKey: 'AIzaSyC7Z4yytbay002dTP3CPqwGHjzq3i34PHk',
+  authDomain: 'cpsc455-notes-app-d5f8a.firebaseapp.com',
+  projectId: 'cpsc455-notes-app-d5f8a',
+  storageBucket: 'cpsc455-notes-app-d5f8a.appspot.com',
+  messagingSenderId: '502456970891',
+  appId: '1:502456970891:web:ef8ab9995d7fa6a6792e8e',
+  measurementId: 'G-TQ706E35P8',
 };
